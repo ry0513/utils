cb3726d9323879d8a9aa8b0a0033fcead787168a
RuoYu <rrcj123456@gmail.com>
2024-12-05 08:33:08 +0000

    新增 『WorkerPools』

--- README.md
+++ README.md
@@ -10,6 +10,77 @@ pnpm i @ry0513/utils
 
 ## 方法
 
-extName 扩展名
+- [extName](#extName)
+- [baseName](#baseName)
 
-baseName 文件名
+## 类
+
+- [WorkerPools](#WorkerPools)
+
+### extName
+
+用于获取文件扩展名
+
+类型
+
+```
+extName(path: string, withDot?: boolean): string
+```
+
+例子
+
+```js
+import { extName } from "@ry0513/utils";
+console.log(extName("/path/AAA00.jpg")); // jpg
+console.log(extName("/path/AAA00.jpg", true)); // .jpg
+```
+
+### baseName
+
+用于获取文件名
+
+类型
+
+```
+baseName(path: string, ext?: boolean): string
+```
+
+例子
+
+```js
+import { baseName } from "@ry0513/utils";
+console.log(baseName("/path/AAA00.jpg")); // AAA00
+console.log(baseName("/path/AAA00.jpg", true)); // AAA00.jpg
+```
+
+### WorkerPools
+
+用于 Web Worker 的连接池，创建实例时不会创建线程；当有新任务加入时会检测存在的线程数，若线程数不超过最大时则会创建新的线程并执行该任务，否则会加入任务队列等待执行；当一个线程完成任务时会检测任务队列，若队列不为空时会按顺序取出下一个任务继续执行（复用该线程），若队列为空则会自动终止
+
+类型
+
+```
+new WorkerPools(path: string | URL, maxWorkers = 4)
+```
+
+例子
+
+```js
+import { WorkerPools } from "@ry0513/utils";
+// 假设 worker.js 计算文件MD5的并返回
+const pools = new WorkerPools("./worker.js", 8);
+// vite 需要 new URL() 导入
+// https://cn.vitejs.dev/guide/features.html#web-workers
+// const pools = new WorkerPools(new URL("./worker", import.meta.url), 8);
+// 假设 fileList 等待计算的文件列表
+fileList.map(async (file) => {
+  try {
+    file.MD5 = await pools.addTask(file);
+  } catch (error) {
+    file.MD5 = null;
+    console.error(error);
+  }
+});
+
+// pools.terminateAll(); // 若有需要，可以直接终止全部线程
+```
---
+++ src/worker.ts
@@ -0,0 +1,70 @@
+/**
+ * Web Worker 线程池
+ * @param path 文件地址 
+ * @param maxWorkers 允许最大的线程数量，默认 4
+ */
+export class WorkerPools {
+    private path: string | URL;
+    private maxWorkers: number;
+    private workers: Worker[] = [];
+    private taskQueue: any[] = [];
+
+    constructor(path: string | URL, maxWorkers = 4) {
+        this.path = path;
+        this.maxWorkers = maxWorkers;
+    }
+
+    private runTask(worker?: Worker) {
+        if (worker || this.workers.length < this.maxWorkers) {
+            worker = worker || this.initialize();
+            if (this.taskQueue.length > 0) {
+                const nextTask = this.taskQueue.shift()!;
+                const listenMessage = (e: MessageEvent) => {
+                    nextTask.resolve(e.data);
+                    this.runTask(worker);
+                    worker!.removeEventListener("message", listenMessage);
+                    worker!.removeEventListener("error", listenError);
+                };
+                const listenError = (e: ErrorEvent) => {
+                    nextTask.reject(e);
+                    this.runTask(worker);
+                    worker!.removeEventListener("message", listenMessage);
+                    worker!.removeEventListener("error", listenError);
+                };
+                worker.addEventListener("error", listenError);
+                worker.addEventListener("message", listenMessage);
+                worker.postMessage(nextTask.data);
+            } else {
+                this.workers = this.workers.filter((t) => t !== worker);
+                worker.terminate();
+            }
+        }
+    }
+
+    private initialize(): Worker {
+        const worker = new Worker(this.path);
+        this.workers.push(worker);
+        return worker;
+    }
+
+    /**
+     * 终止全部线程
+     */
+    public terminateAll(): void {
+        this.workers.forEach((worker) => worker.terminate());
+        this.workers = [];
+        this.taskQueue = [];
+    }
+
+    /**
+     * 添加任务
+     * @param data 
+     * @returns 
+     */
+    public addTask(data: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.taskQueue.push({ data, resolve, reject });
+            this.runTask();
+        });
+    }
+}
