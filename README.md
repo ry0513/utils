9b0d79a90746cb6bf09f9c4cf3be2bdb2237e0a1
RuoYu <rrcj123456@gmail.com>
2024-12-06 15:22:49 +0000

    『WorkerPools』更名为『Worker』

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ pnpm i @ry0513/utils
 
 ## 类
 
-- [WorkerPools](#WorkerPools)
+- [Worker](#Worker)
 
 ### extName
 
@@ -53,14 +53,14 @@ console.log(baseName("/path/AAA00.jpg")); // AAA00
 console.log(baseName("/path/AAA00.jpg", true)); // AAA00.jpg
 ```
 
-### WorkerPools
+### Worker
 
 用于 Web Worker 的连接池，创建实例时不会创建线程；当有新任务加入时会检测存在的线程数，若线程数不超过最大时则会创建新的线程并执行该任务，否则会加入任务队列等待执行；当一个线程完成任务时会检测任务队列，若队列不为空时会按顺序取出下一个任务继续执行（复用该线程），若队列为空则会自动终止
 
 类型
 
 ```
-new WorkerPools(path: string | URL, options?:WorkerPoolsOptions)
+new Worker(path: string | URL, options?:WorkerPoolsOptions)
 
 type WorkerPoolsOptions = {
     maxWorkers?: number;
@@ -72,15 +72,15 @@ type WorkerPoolsOptions = {
 例子
 
 ```js
-import { WorkerPools } from "@ry0513/utils";
+import { Worker } from "@ry0513/utils";
 // 假设 worker.js 计算文件MD5的并返回
-const pools = new WorkerPools("./worker.js", {
+const pools = new Worker("./worker.js", {
   maxWorkers: 8,
   type: "module",
 });
 // vite 需要 new URL() 导入
 // https://cn.vitejs.dev/guide/features.html#web-workers
-// const pools = new WorkerPools(new URL("./worker", import.meta.url), {
+// const pools = new Worker(new URL("./worker", import.meta.url), {
 //  maxWorkers: 8,
 //  type: "module"
 //});
--- src/worker.ts
+++ src/worker.ts
@@ -1,81 +1,135 @@
 type WorkerPoolsOptions = {
-    maxWorkers?: number;
-    credentials?: WorkerOptions["credentials"];
-    type?: WorkerOptions["type"];
+  maxWorkers?: number;
+  credentials?: WorkerOptions["credentials"];
+  type?: WorkerOptions["type"];
 };
+
+type workerType = import("worker_threads").Worker | globalThis.Worker;
 
 /**
  * Web Worker 线程池
  * @param path 文件地址
  * @param maxWorkers 允许最大的线程数量，默认 4
  */
-export class WorkerPools {
-    private path: string | URL;
-    private maxWorkers: number;
-    private workers: Worker[] = [];
-    private taskQueue: any[] = [];
-    private WorkerPoolsOptions: WorkerPoolsOptions;
+export class Worker {
+  private path: string | URL;
+  private maxWorkers: number;
+  private workers: workerType[] = [];
+  private taskQueue: any[] = [];
+  private WorkerPoolsOptions: WorkerPoolsOptions;
+  private isBrowser: boolean;
+  constructor(path: string | URL, WorkerPoolsOptions: WorkerPoolsOptions = {}) {
+    this.path = path;
+    this.maxWorkers = WorkerPoolsOptions.maxWorkers || 4;
+    this.WorkerPoolsOptions = WorkerPoolsOptions;
+    this.isBrowser = typeof window !== "undefined";
+  }
 
-    constructor(path: string | URL, WorkerPoolsOptions: WorkerPoolsOptions = {}) {
-        this.path = path;
-        this.maxWorkers = WorkerPoolsOptions.maxWorkers || 4;
-        this.WorkerPoolsOptions = WorkerPoolsOptions;
+  private async initialize(): Promise<workerType> {
+    let worker: workerType;
+    if (this.isBrowser) {
+      worker = new window.Worker(this.path, {
+        type: this.WorkerPoolsOptions.type,
+        credentials: this.WorkerPoolsOptions.credentials,
+      });
+    } else {
+      const { Worker } = await import("worker_threads");
+      worker = new Worker(this.path);
     }
 
-    private runTask(worker?: Worker) {
-        if (worker || this.workers.length < this.maxWorkers) {
-            worker = worker || this.initialize();
-            if (this.taskQueue.length > 0) {
-                const nextTask = this.taskQueue.shift()!;
-                const listenMessage = (e: MessageEvent) => {
-                    nextTask.resolve(e.data);
-                    this.runTask(worker);
-                    worker!.removeEventListener("message", listenMessage);
-                    worker!.removeEventListener("error", listenError);
-                };
-                const listenError = (e: ErrorEvent) => {
-                    nextTask.reject(e);
-                    this.runTask(worker);
-                    worker!.removeEventListener("message", listenMessage);
-                    worker!.removeEventListener("error", listenError);
-                };
-                worker.addEventListener("error", listenError);
-                worker.addEventListener("message", listenMessage);
-                worker.postMessage(nextTask.data);
-            } else {
-                this.workers = this.workers.filter((t) => t !== worker);
-                worker.terminate();
-            }
+    this.workers.push(worker);
+    return worker;
+  }
+
+  private async runTask(worker?: workerType) {
+    if (worker || this.workers.length < this.maxWorkers) {
+      if (!worker) {
+        worker = await this.initialize();
+      }
+      if (this.taskQueue.length > 0) {
+        const nextTask = this.taskQueue.shift()!;
+        if ("removeEventListener" in worker) {
+          const listenMessage = (e: MessageEvent) => {
+            nextTask.resolve(e.data);
+            this.runTask(worker);
+            (worker as globalThis.Worker).removeEventListener(
+              "message",
+              listenMessage
+            );
+            (worker as globalThis.Worker).removeEventListener(
+              "error",
+              listenError
+            );
+          };
+          const listenError = (e: ErrorEvent) => {
+            nextTask.reject(e);
+            this.runTask(worker);
+            (worker as globalThis.Worker).removeEventListener(
+              "message",
+              listenMessage
+            );
+            (worker as globalThis.Worker).removeEventListener(
+              "error",
+              listenError
+            );
+          };
+          worker.addEventListener("error", listenError);
+          worker.addEventListener("message", listenMessage);
+        } else {
+          const listenMessage = (data: any) => {
+            nextTask.resolve(data);
+            this.runTask(worker);
+            (worker as import("worker_threads").Worker).removeListener(
+              "message",
+              listenMessage
+            );
+            (worker as import("worker_threads").Worker).removeListener(
+              "error",
+              listenError
+            );
+          };
+          const listenError = (e: ErrorEvent) => {
+            nextTask.reject(e);
+            this.runTask(worker);
+            (worker as import("worker_threads").Worker).removeListener(
+              "message",
+              listenMessage
+            );
+            (worker as import("worker_threads").Worker).removeListener(
+              "error",
+              listenError
+            );
+          };
+          worker.addListener("error", listenError);
+          worker.addListener("message", listenMessage);
         }
-    }
 
-    private initialize(): Worker {
-        const worker = new Worker(this.path, {
-            type: this.WorkerPoolsOptions.type,
-            credentials: this.WorkerPoolsOptions.credentials
-        });
-        this.workers.push(worker);
-        return worker;
+        worker.postMessage(nextTask.data);
+      } else {
+        this.workers = this.workers.filter((t) => t !== worker);
+        worker.terminate();
+      }
     }
+  }
 
-    /**
-     * 终止全部线程
-     */
-    public terminateAll(): void {
-        this.workers.forEach((worker) => worker.terminate());
-        this.workers = [];
-        this.taskQueue = [];
-    }
+  /**
+   * 添加任务
+   * @param data
+   * @returns
+   */
+  public addTask(data: any): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      this.taskQueue.push({ data, resolve, reject });
+      await this.runTask();
+    });
+  }
 
-    /**
-     * 添加任务
-     * @param data
-     * @returns
-     */
-    public addTask(data: any): Promise<any> {
-        return new Promise((resolve, reject) => {
-            this.taskQueue.push({ data, resolve, reject });
-            this.runTask();
-        });
-    }
+  /**
+   * 终止全部线程
+   */
+  public terminateAll(): void {
+    this.workers.forEach((worker) => worker.terminate());
+    this.workers = [];
+    this.taskQueue = [];
+  }
 }
